4c18d167b13e86ab6139850a922907b0cdc54c81
Midjan <midjankacirani@gmail.com>
2024-04-19 03:40:26 +0200

    Perfundim laborator 4

--- Laborator4/Laborator4/src/test/java/testimui.java
+++ Laborator4/Laborator4/src/test/java/testimui.java
@@ -1,6 +1,4 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,9 +8,21 @@ import org.openqa.selenium.support.ui.Select;
 import java.time.Duration;
 import java.util.Objects;
 
+/**
+ * Shenim:
+ * Ne varesi te rrjedhes, jane krijuar tre teste. Sigursht kane varesi ndaj njera tjetres, prandaj eshte e rendesishme qe te ekzekutohen ne nje radhe te percaktuar.
+ * Per te evidentuar kete, mund te shtohet nje kusht qe kontrollohet URL e faqes, dhe nqs jena ne faqen e duhur te behen asserts, nqs jo te behet nje assertFalse qe te bej fail testi.
+ * Gjithesesi, nuk eshte ky qellim i laboratorit por testimi i website.
+ */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class testimui {
 
     static WebDriver driver;
+    static String firstName;
+    static String lastName;
+    static String email;
+    static String passwordStr;
+    static String kompani;
 
     @BeforeAll
     public static void init() {
@@ -21,19 +31,22 @@ public class testimui {
         driver.manage().window().maximize();
 
         //Inicializojme variablat qe do te perdoren gjate metodave
-
+        firstName = "FirstName" + System.currentTimeMillis() / 1000L;
+        lastName = "LastName" + System.currentTimeMillis() / 1000L;
+        email = "email" + System.currentTimeMillis() / 1000L + "@dummyemail.com";
+        passwordStr = "password" + System.currentTimeMillis() / 1000L;
+        kompani = "Kompani " + System.currentTimeMillis() / 1000L + " CO.";
     }
 
-//    @AfterAll
-//    public static void destroy() {
-//        driver.close();
-//    }
-
+    @AfterAll
+    public static void destroy() {
+        driver.close();
+    }
 
     @Test
+    @Order(1)
     public void ex1234() {
-        //body > div.master-wrapper-page > div.header > div.header-upper > div.header-links-wrapper > div.header-links > ul > li:nth-child(2) > a
-        //Navigoni ne
+        //Navigoni ne https://demo.nopcommerce.com/
         driver.get("https://demo.nopcommerce.com/");
         driver.findElement(By.className("ico-login")).click();
         System.out.println(driver.findElement(By.className("register-button")).getText());
@@ -44,6 +57,7 @@ public class testimui {
     }
 
     @Test
+    @Order(2)
     public void ex56() throws InterruptedException {
         //Ruajme radio buttons
         WebElement maleRadioButton = driver.findElement(By.id("gender-male"));
@@ -57,7 +71,7 @@ public class testimui {
         Assertions.assertTrue(maleRadioButton.isSelected() && !femaleRadioButton.isSelected());
 
         Thread.sleep(2000);
-        String firstName = "FirstName" + System.currentTimeMillis() / 1000L;
+
 
         driver.findElement(By.id("FirstName")).sendKeys(firstName);
 
@@ -78,10 +92,10 @@ public class testimui {
         Assertions.assertFalse(Objects.equals(date.getFirstSelectedOption().getText(), date.getOptions().get(0).getText()) || Objects.equals(month.getFirstSelectedOption().getText(), month.getOptions().get(0).getText()) || Objects.equals(year.getFirstSelectedOption().getText(), year.getOptions().get(0).getText()));
 
         //Plotesojme email
-        driver.findElement(By.id("Email")).sendKeys("email" + System.currentTimeMillis() / 1000L + "@dummyemail.com");
+        driver.findElement(By.id("Email")).sendKeys(email);
 
         //Plotesojme kompanine
-        driver.findElement(By.id("Company")).sendKeys("Kompani " + System.currentTimeMillis() / 1000L + " CO.");
+        driver.findElement(By.id("Company")).sendKeys(kompani);
 
         //E hekum opsionin per te marr te rejat/newsletter
         driver.findElement(By.id("Newsletter")).click();
@@ -110,7 +124,7 @@ public class testimui {
         //Fshijme vleren e meparshme
         password.clear();
         //Vendosim vleren e re
-        password.sendKeys("123456");
+        password.sendKeys(passwordStr);
 
 
         confirmPassword.sendKeys("nonMatchingPassword");
@@ -122,19 +136,36 @@ public class testimui {
         //Fshijme vleren e meparshme
         confirmPassword.clear();
         //Vendosim vleren e re
-        confirmPassword.sendKeys("123456");
+        confirmPassword.sendKeys(passwordStr);
 
         Thread.sleep(2000);
 
         driver.findElement(By.cssSelector("div[class='buttons']>button")).click();
 
-        Thread.sleep(2000);
-
         Assertions.assertEquals("Your registration completed", driver.findElement(By.className("result")).getText());
-
+        Thread.sleep(2000);
         //Klikojme continue per te vazhduar ne fillim qe te bejme log-in.
-        driver.findElement(By.className("button-1 register-continue-button")).click();
+        driver.findElement(By.className("register-continue-button")).click();
+
+        Thread.sleep(2000);
     }
 
+    @Test
+    @Order(3)
+    public void ex8910() throws InterruptedException {
+        //Klikojme Log in
+        driver.findElement(By.className("ico-login")).click();
+
+        //Plotesojme fushat
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys(email);
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys(passwordStr);
+
+        //Klikojme login
+        driver.findElement(By.className("login-button")).click();
+
+        Assertions.assertEquals("nopCommerce demo store", driver.getTitle());
+        Assertions.assertTrue(driver.findElement(By.className("ico-logout")).isDisplayed());
+
+    }
 
 }
